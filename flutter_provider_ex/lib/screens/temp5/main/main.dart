4fb1cdef5a085fc1ac08f27bd6db1008a65422aa
DESKTOP-KAL0F0T\Gia Le <lhgia.hgi@vnpt.vn>
2021-11-07 23:07:09 +0700

    push

--- flutter_provider_ex/lib/screens/temp5/main/main.dart
+++ flutter_provider_ex/lib/screens/temp5/main/main.dart
@@ -22,41 +22,35 @@ class _MainScreen5State extends State<MainScreen5>
   var apiController = Get.put(API_Manager());
   late GlobalKey<AnimatedListState> _keyList = GlobalKey<AnimatedListState>();
 
-  late final AnimationController _controller = AnimationController(
-    duration: Duration(milliseconds: 500),
-    vsync: this,
-  )..forward();
-  late final Animation<Offset> _offsetAnimation = Tween<Offset>(
-    begin: const Offset(1, 0),
-    end: const Offset(0, 0),
-  ).animate(CurvedAnimation(
-    parent: _controller,
-    curve: Curves.easeInCirc,
-  ));
+  late final AnimationController animationController;
+  Duration animationDuration = const Duration(milliseconds: 270);
+  late final Animation<Offset> _offsetAnimation;
+  late Tween<Offset> _offset;
 
   @override
   void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
     apiController.getTrips();
     tripRefreshController = RefreshController();
 
-    // _controller = AnimationController(
-    //   duration: Duration(milliseconds: 500),
-    //   vsync: this,
-    // )..forward();
+    animationController =
+        AnimationController(duration: animationDuration, vsync: this)
+          ..forward();
 
-    // _offsetAnimation = Tween<Offset>(
-    //   begin: const Offset(1, 0),
-    //   end: const Offset(0, 0),
-    // ).animate(CurvedAnimation(
-    //   parent: _controller,
-    //   curve: Curves.easeInCirc,
-    // ));
+    _offset = Tween(begin: const Offset(1, 0), end: const Offset(0, 0));
 
-    super.initState();
+    // _offsetAnimation1.drive(_offset);
+    _offsetAnimation =
+        Tween<Offset>(begin: const Offset(1, 0), end: const Offset(0, 0))
+            .animate(
+      CurvedAnimation(parent: animationController, curve: Curves.easeInCirc),
+    );
   }
 
   @override
   void dispose() {
+    animationController.dispose();
     tripRefreshController.dispose();
     apiController.dispose();
     super.dispose();
@@ -65,8 +59,6 @@ class _MainScreen5State extends State<MainScreen5>
   @override
   Widget build(BuildContext context) {
     Size _size = MediaQuery.of(context).size;
-    Tween<Offset> _offset =
-        Tween(begin: const Offset(1, 0), end: const Offset(0, 0));
     // apiController.tripData.forEach((element) {
     //   _keyList.currentState!.insertItem(apiController.tripData.length - 1);
     // });
@@ -76,10 +68,10 @@ class _MainScreen5State extends State<MainScreen5>
       child: GetBuilder<API_Manager>(
         builder: (_) {
           if (apiController.tripData.length > 0) {
-            for (var element in apiController.tripData) {
-              _keyList.currentState
-                  ?.insertItem(apiController.tripData.length - 1);
-            }
+            // for (var element in apiController.tripData) {
+            //   _keyList.currentState
+            //       ?.insertItem(apiController.tripData.length - 1);
+            // }
             return SmartRefresher(
               controller: tripRefreshController,
               onRefresh: () {
@@ -109,30 +101,30 @@ class _MainScreen5State extends State<MainScreen5>
               footer: CustomFooterTrip(),
               enablePullUp: true,
               enablePullDown: true,
-              // child: AnimatedList(
-              //   key: _keyList,
-              //   initialItemCount: apiController.tripData.length,
-              //   itemBuilder: (context, index, animation) {
-              //     final trip = apiController.tripData[index];
-              //     return SlideTransition(
-              //       child: TripList(trip: trip),
-              //       position: _offsetAnimation,
-              //     );
-              //   },
-              // ),
-              child: ListView.separated(
-                itemBuilder: (context, index) {
+              child: AnimatedList(
+                key: _keyList,
+                initialItemCount: apiController.tripData.length,
+                itemBuilder: (context, index, animation) {
                   final trip = apiController.tripData[index];
-                  // apiController.loadTime = apiController.loadTime + 20;
                   return SlideTransition(
-                    position: _offsetAnimation,
                     child: TripList(trip: trip),
+                    position: animation.drive(_offset),
                   );
                 },
-                separatorBuilder: (BuildContext context, int val) =>
-                    const Divider(),
-                itemCount: apiController.tripData.length,
               ),
+              // child: ListView.separated(
+              //   itemBuilder: (context, index) {
+              //     final trip = apiController.tripData[index];
+              //     // apiController.loadTime = apiController.loadTime + 20;
+              //     return SlideTransition(
+              //       position: _offsetAnimation,
+              //       child: TripList(trip: trip),
+              //     );
+              //   },
+              //   separatorBuilder: (BuildContext context, int val) =>
+              //       const Divider(),
+              //   itemCount: apiController.tripData.length,
+              // ),
             );
           } else
             return const Center(child: CircularProgressIndicator());
