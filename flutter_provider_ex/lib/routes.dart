ed68d65f7f28d367f6875c22b2a55c156c84f1db
DESKTOP-KAL0F0T\Gia Le <lhgia.hgi@vnpt.vn>
2021-11-09 22:38:46 +0700

    push

--- flutter_provider_ex/lib/routes.dart
+++ flutter_provider_ex/lib/routes.dart
@@ -1,3 +1,4 @@
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_provider_ex/screens/contact/contact.dart';
 import 'package:flutter_provider_ex/screens/temp1/detail1/detail.dart';
@@ -193,6 +194,18 @@ class RouteManager {
             ],
           ),
         ),
+        // bottomNavigationBar: ConvexAppBar(
+        //   style: TabStyle.react,
+        //   items: const [
+        //     TabItem(icon: Icons.home, title: 'Home'),
+        //     TabItem(icon: Icons.map, title: 'Discovery'),
+        //     TabItem(icon: Icons.add, title: 'Add'),
+        //     TabItem(icon: Icons.message, title: 'Message'),
+        //     TabItem(icon: Icons.people, title: 'Profile'),
+        //   ],
+        //   initialActiveIndex: 2, //optional, default as 0
+        //   onTap: (int i) => print('click index=$i'),
+        // ),
       );
     }
     return result;
